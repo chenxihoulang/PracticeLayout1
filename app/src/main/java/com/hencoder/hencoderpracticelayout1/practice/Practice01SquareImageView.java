328e7fe47e0f7b9f0511ffc5c637bf4bef029bff
chaihongwei <714652017@qq.com>
2017-11-08 15:51:11 +0800

    master add 添加测试代码

--- app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
+++ app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
@@ -29,9 +29,15 @@ public class Practice01SquareImageView extends ImageView {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
         // 先用 getMeasuredWidth() 和 getMeasuredHeight() 取到 super.onMeasure() 的计算结果
+        int width=getMeasuredWidth();
+        int height=getMeasuredHeight();
+
+        int squareSize=Math.min(width,height);
 
         // 然后通过计算，让宽度和高度一致
 
         // 再用 setMeasuredDimension(width, height) 来保存最终的宽度和高度
+
+        setMeasuredDimension(squareSize,squareSize);
     }
 }
